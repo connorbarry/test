c85edbc7b30dca6a62adfe68f64fa65f3c9f1f3d
deonmcbride <deiante.mcbride@bobcats.gcsu.edu>
2020-01-14 16:50:50 -0500

    I added a 3rd number

--- Testex.py
+++ Testex.py
@@ -1,4 +1,5 @@
-Num1 = input("Give me a number:")
-Num2 = input("Give me another number:")
-Total = Num1 + Num2
+Num1 = input("Give me a number: ")
+Num2 = input("Give me another number: ")
+Num3 = input("Give number 3: " )
+Total = Num1 + Num2 + Num3
 print("This is your total: " + Total)
